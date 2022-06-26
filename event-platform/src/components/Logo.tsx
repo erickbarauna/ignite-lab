0a8dd52ad6160ad7efb0b6596ac3572469b87e41
erickbarauna <ericksbarauna26@gmail.com>
2022-06-26 13:11:52 -0300

    .

--- event-platform/src/components/Logo.tsx
+++ event-platform/src/components/Logo.tsx
@@ -2,7 +2,7 @@ export function Logo() {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      width="239"
+      width="270"
       height="34"
       fill="none"
       viewBox="0 0 239 34"
--- event-platform/src/components/Video.tsx
+++ event-platform/src/components/Video.tsx
@@ -6,8 +6,8 @@ import '@vime/core/themes/default.css';
 
 
 const GET_LESSON_BY_SLUG_QUERY = gql `
-  query GetLessonBySlug ($slug: String){ 
-    lesson(where: {slug: "$slug"}) {
+  query GetLessonBySlug ($slug: String) { 
+    lesson(where: {slug: $slug}) {
       title
       videoId
       description
@@ -20,7 +20,7 @@ const GET_LESSON_BY_SLUG_QUERY = gql `
   }
 `
 
-interface GetLessonBySlug {
+interface GetLessonQueryResponse {
     lesson: {
       title: string;
       videoId: string;
@@ -38,7 +38,7 @@ interface VideoProps {
 }
 
 export function Video(props: VideoProps) {
-  const { data } = useQuery<GetLessonBySlug>(GET_LESSON_BY_SLUG_QUERY, {
+  const { data } = useQuery(GET_LESSON_BY_SLUG_QUERY, {
     variables: {
       slug: props.lessonSlug,
     }
@@ -54,10 +54,10 @@ export function Video(props: VideoProps) {
 
   return (
     <div className="flex-1">
-      <div className="bg-black flex justify-center">
-        <div className="h-full w-full max-w[1100px] max-h-[60vh] aspect-video">
+      <div className="bg-gray-700 flex justify-center">
+        <div className="h-full w-full max-w[800px] max-h-[60vh] aspect-video">
           <Player>
-            <Youtube videoId="data.lesson.videoId"/>
+            <Youtube videoId={data.lesson.videoId}/>
             <DefaultUi />
           </Player>
         </div>
@@ -77,11 +77,11 @@ export function Video(props: VideoProps) {
             <div className="flex items-center gap-4 mt-6">
               <img 
                 className="h-16 w-16 rounded-full border-2 border-blue-500"
-                src="data.lesson.teacher.avatarURL"
+                src={data.lesson.teacher.avatarURL}
                 alt="" />
               <div className="leading-relaxed">
-                <strong className="font-bold text-2xl block">data.lesson.teacher.name</strong>
-                <span className="text-gray-200 text-sm block">data.lesson.teacher.bio</span>
+                <strong className="font-bold text-2xl block">{data.lesson.teacher.name}</strong>
+                <span className="text-gray-200 text-sm block">{data.lesson.teacher.bio}</span>
               </div>
             </div>
           </div>
