6b227f0650142e34660c25e637b24dbe7279d930
erickbarauna <ericksbarauna26@gmail.com>
2022-07-04 11:00:09 -0300

    aula 4 finalizada ("eu espero")

--- event-platform/src/pages/Subscribe.tsx
+++ event-platform/src/pages/Subscribe.tsx
@@ -1,27 +1,53 @@
-import { useState } from "react";
+import { gql, useMutation } from "@apollo/client";
+import { useState, FormEvent } from "react";
+import { useNavigate } from "react-router-dom";
 import { Logo } from "../components/Logo";
 
+const CREATE_SUBSCRIBER_MUTATION = gql`
+  mutation CreateSubscriber ($name: String!, $email: String!) {
+    createSubscriber(data: {name: $name, email: $email}) {
+      id
+    }
+  }
+`
 
 export function Subscribe() {
-  const [name, setName]
+  const navigate = useNavigate()
+
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+
+  const [createSubscriber, { loading }] = useMutation(CREATE_SUBSCRIBER_MUTATION)
+
+  async function handleSubscribe(event: FormEvent) {
+    event.preventDefault();
+
+    await createSubscriber({
+      variables: {
+        name,
+        email,
+      }
+    })
+
+    navigate('/event')
+  }
 
   return (
     <div className="min-h-screen bg-blur bg-cover bg-no-repeat flex flex-col items-center">
-      <div className="w-full max-w-[1100px] h-[450px] flex items-center justify-between mt-20 mx-auto bg-icon bg-no-repeat bg-top" >
-        <div className="max-w-[640px]">
+      <div className="w-full max-w-[1100px] h-[450px] flex items-center justify-between mt-20 mx-auto">
+        <div className="maz-w-[648px]">
           <Logo />
-        
           <h1 className="mt-8 text-[2.5rem] leading-tight">
             Construa uma <strong className="text-blue-500">aplicação completa</strong>, do zero, com <strong className="text-blue-500">React JS</strong>
           </h1>
           <p className="mt-4 text-gray-200 leading-relaxed">
             Em apenas uma semana você vai dominar na prática uma das tecnologias mais utilizadas e com alta demanda para acessar as melhores oprtunidades do mercado.
           </p>
-        <div/>
+        </div>
 
         <div className="p-8 bg-gray-700 border border-gray-500 rounded">
           <strong className="text-2xl mb-6 block">Inscreva-se gratuitamente</strong>
-            <form action="" className="flex flex-col gap-2 w-full">
+          <form onSubmit={handleSubscribe} className="flex flex-col gap-2 w-full">
               <input 
                 className="bg-gray-900 rounded px-5 h-14"
                 type="text" 
@@ -37,15 +63,14 @@ export function Subscribe() {
 
               <button 
                 type="submit"
-                className="mt-4 bg-green-500 uppercase py-4 rounded font-bold text-sm hover:bg-green-700 transition-colors"
+                disabled={loading}
+                className="mt-4 bg-green-500 uppercase py-4 rounded font-bold text-sm hover:bg-green-700 transition-colors disable:opacity-50"
               >
                 Garantir minha vaga
               </button>
             </form>
         </div>
       </div>
-
-      <img src="/src/assets/code-mockup.png" alt="mt-10" />
     </div>
-  );
-}  
+  )
+}
